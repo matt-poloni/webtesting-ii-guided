94de21e255e3fa090ec74a7ed0d75a5612f8d642
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-05-14 13:28:46 -0400

    Tests firing of click event

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
-import { render } from 'react-testing-library';
+import { render, fireEvent } from 'react-testing-library';
+import 'react-testing-library/cleanup-after-each';
 import App from './App';
 
 describe('<App />', () => {
   it('renders fine', () => {
     render(<App />);
+  })
+
+  it('should display hello world', () => {
+    const { getByText } = render(<App />);
+    getByText(/hello world!/i);
+  })
+
+  it('should greet the team', () => {
+    const { getByText } = render(<App />);
+    const button = getByText(/greet/i);
+    fireEvent.click(button);
+    getByText(/hello web 18/i);
   })
 })
